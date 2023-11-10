05860a1d87c3d88e5e582b2603a34d933cb1cbee
Maria Jarv <118188337+Sunmarli@users.noreply.github.com>
2023-11-10 12:45:46 +0200

    added font

--- Calculator/Calculator/App.xaml.cs
+++ Calculator/Calculator/App.xaml.cs
@@ -1,11 +1,31 @@
-﻿namespace Calculator;
+﻿using Views;
 
+namespace Calculator;
+#if WINDOWS
+using Microsoft.UI;
+using Microsoft.UI.Windowing;
+using Windows.Graphics;
+#endif
 public partial class App : Application
+
 {
+	const int WindowWidth = 540;
+	const int WindowHeight = 900;
 	public App()
 	{
 		InitializeComponent();
+#if WINDOWS
+		Microsoft.Maui.Handlers.WindowHandler.Mapper.AppendToMapping(nameof(IWindow), (handler, view)=>
+		{var mauiWindow = handler.VirtualView;
+            var nativeWindow = handler.PlatformView;
+            nativeWindow.Activate();
+            IntPtr windowHandle = WinRT.Interop.WindowNative.GetWindowHandle(nativeWindow);
+            WindowId windowId = Microsoft.UI.Win32Interop.GetWindowIdFromWindow(windowHandle);
+            AppWindow appWindow = Microsoft.UI.Windowing.AppWindow.GetFromWindowId(windowId);
+            appWindow.Resize(new SizeInt32(WindowWidth, WindowHeight));
+		});
+#endif
 
-		MainPage = new AppShell();
+		MainPage = new CalculatorPage();
 	}
 }
